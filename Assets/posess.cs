c8990eb604b891ae236cad6b26f58a8def706a68
hakr1337 <jabrad44@gmail.com>
2015-09-18 10:03:06 -0600

    first push
    basic posession

---
+++ Assets/posess.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class posess : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (c.GetComponent<Posessable>()) {
+			Camera.main.transform.parent = c.transform;
+			Camera.main.transform.localPosition = Vector3.zero;
+
+			gameObject.GetComponent<CharacterController>().enabled = false;
+			gameObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+
+			Camera.main.transform.localRotation = c.transform.localRotation;
+
+			c.gameObject.GetComponent<CharacterController>().enabled = true;
+			c.gameObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+
+			c.GetComponent<BoxCollider>().isTrigger = false;
+			c.GetComponent<ParticleSystem>().enableEmission = false;
+			Camera.main.gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
+		}
+	}
+}
