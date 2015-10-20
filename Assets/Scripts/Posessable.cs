1d92a742c1acec42417da1a90c9459241f5958eb
hakr1337 <jabrad44@gmail.com>
2015-10-20 14:06:55 -0600

    prototype final

--- Assets/Scripts/Posessable.cs
+++ Assets/Scripts/Posessable.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Posessable : MonoBehaviour {
 	public bool posessed = false;
+	public bool shouldScare = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,12 +11,20 @@ public class Posessable : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if((Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.W)||
+		    Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S)||
+		    Input.GetKeyDown(KeyCode.D))&& posessed){
+			shouldScare = !shouldScare;
+		}
+
 		if (Input.GetKey(KeyCode.Escape) && posessed){
 			GameObject.Find("Player").GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
 			GameObject.Find("Player").GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().enabled = true;
 			GameObject.Find("Player").GetComponent<Rigidbody>().isKinematic = false;
+			GameObject.Find("Player").GetComponent<CapsuleCollider>().enabled = true;
 			this.GetComponent<Collider>().isTrigger = true;
 			posessed = false;
+
 
 			if(gameObject.tag == "balltrigger"){
 				gameObject.GetComponentInParent<ball>().deposess();
--- Assets/Scripts/posess.cs
+++ Assets/Scripts/posess.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class posess : MonoBehaviour {
 
-	GameObject[] posessables = new GameObject[5];
+	GameObject[] posessables = new GameObject[15];
 	// Use this for initialization
 	void Start () {
 		
@@ -16,34 +16,22 @@ public class posess : MonoBehaviour {
 			
 		//}
 	}
-
-	/*void OnTriggerEnter(Collider c){
-
-
-
-
-
-
-		//if (Input.GetKey (KeyCode.Q)) {
-			if (c.GetComponent<Posessable> ()) {
-				//this.gameObject.SetActive(false);
-				for(int i = 0; i < 5; i++){
-				 	if(posessables[i] == null){
-						posessables[i] = c;
-					}
-				}
-			}
-		//}
-	}*/
+	
 
 	void OnTriggerStay(Collider c){
 		//print (c.name);
-		if (Input.GetKeyDown (KeyCode.Q)) {
+		if (Input.GetKeyDown (KeyCode.Q) && c.GetComponent<Posessable>() != null) {
+			if(Camera.main.GetComponent<cameraStuff>().visionOn){
+				Camera.main.GetComponent<cameraStuff>().turnOff();
+			}
+
 			c.GetComponent<Collider>().isTrigger = false;
 			c.GetComponent<Posessable>().posessed = true;
+
 			this.gameObject.GetComponentInChildren<SkinnedMeshRenderer> ().enabled = false;
 			this.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().enabled = false;
 			this.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+			this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
 		}
 	}
 }
